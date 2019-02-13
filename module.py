79799e4e3d80fc8e6edf9080220d6f9ea4fa0635
Samira Munir <42713239+samiramunir@users.noreply.github.com>
2019-02-13 12:15:57 -0500

    uploading project files

---
+++ module.py
@@ -0,0 +1,163 @@
+import nltk
+import random
+#from nltk.corpus import movie_reviews
+from nltk.classify.scikitlearn import SklearnClassifier
+import pickle
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.svm import SVC, LinearSVC, NuSVC
+from nltk.classify import ClassifierI
+from statistics import mode
+from nltk.tokenize import word_tokenize
+import os
+
+
+
+class VoteClassifier(ClassifierI):
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf
+print('reading pos files')
+files_pos = os.listdir('/Users/samiramunir/Documents/Flatiron_2018/Blogs /Blog 4/aclImdb/train/pos')
+files_pos = [open('train/pos/'+f, 'r').read() for f in files_pos]
+print('reading neg files')
+files_neg = os.listdir('/Users/samiramunir/Documents/Flatiron_2018/Blogs /Blog 4/aclImdb/train/neg')
+files_neg = [open('train/neg/'+f, 'r').read() for f in files_neg]
+
+
+# short_pos = open("train/positive.txt","r").read()
+# short_neg = open("short_reviews/negative.txt","r").read()
+
+# move this up here
+all_words = []
+documents = []
+
+
+#  j is adject, r is adverb, and v is verb
+#allowed_word_types = ["J","R","V"]
+allowed_word_types = ["J"]
+print('parsing pos')
+for rev in files_pos:
+    documents.append( (rev, "pos") )
+    words = word_tokenize(rev)
+    pos = nltk.pos_tag(words)
+    for w in pos:
+        if w[1][0] in allowed_word_types:
+            all_words.append(w[0].lower())
+print('parsing neg')
+for rev in files_neg:
+    documents.append( (rev, "neg") )
+    words = word_tokenize(rev)
+    pos = nltk.pos_tag(words)
+    for w in pos:
+        if w[1][0] in allowed_word_types:
+            all_words.append(w[0].lower())
+
+
+
+
+save_documents = open("pickled_algos/documents.pickle","wb")
+pickle.dump(documents, save_documents)
+save_documents.close()
+
+
+all_words = nltk.FreqDist(all_words)
+
+
+word_features = list(all_words.keys())[:5000]
+
+
+save_word_features = open("pickled_algos/word_features5k.pickle","wb")
+pickle.dump(word_features, save_word_features)
+save_word_features.close()
+
+
+def find_features(document):
+    words = word_tokenize(document)
+    features = {}
+    for w in word_features:
+        features[w] = (w in words)
+
+    return features
+print('building featuresets')
+featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+random.shuffle(featuresets)
+print(len(featuresets))
+
+testing_set = featuresets[20000:]
+training_set = featuresets[:20000]
+
+print('running Naive b clf')
+classifier = nltk.NaiveBayesClassifier.train(training_set)
+print("Original Naive Bayes Algo accuracy percent:", (nltk.classify.accuracy(classifier, testing_set))*100)
+classifier.show_most_informative_features(15)
+
+###############
+save_classifier = open("pickled_algos/originalnaivebayes5k.pickle","wb")
+pickle.dump(classifier, save_classifier)
+save_classifier.close()
+print('running MNB clf')
+MNB_classifier = SklearnClassifier(MultinomialNB())
+MNB_classifier.train(training_set)
+print("MNB_classifier accuracy percent:", (nltk.classify.accuracy(MNB_classifier, testing_set))*100)
+
+save_classifier = open("pickled_algos/MNB_classifier5k.pickle","wb")
+pickle.dump(MNB_classifier, save_classifier)
+save_classifier.close()
+print('running Bernoulli clf')
+BernoulliNB_classifier = SklearnClassifier(BernoulliNB())
+BernoulliNB_classifier.train(training_set)
+print("BernoulliNB_classifier accuracy percent:", (nltk.classify.accuracy(BernoulliNB_classifier, testing_set))*100)
+
+save_classifier = open("pickled_algos/BernoulliNB_classifier5k.pickle","wb")
+pickle.dump(BernoulliNB_classifier, save_classifier)
+save_classifier.close()
+
+print('running logreg clf')
+LogisticRegression_classifier = SklearnClassifier(LogisticRegression())
+LogisticRegression_classifier.train(training_set)
+print("LogisticRegression_classifier accuracy percent:", (nltk.classify.accuracy(LogisticRegression_classifier, testing_set))*100)
+
+save_classifier = open("pickled_algos/LogisticRegression_classifier5k.pickle","wb")
+pickle.dump(LogisticRegression_classifier, save_classifier)
+save_classifier.close()
+
+print('running linSVC clf')
+LinearSVC_classifier = SklearnClassifier(LinearSVC())
+LinearSVC_classifier.train(training_set)
+print("LinearSVC_classifier accuracy percent:", (nltk.classify.accuracy(LinearSVC_classifier, testing_set))*100)
+
+save_classifier = open("pickled_algos/LinearSVC_classifier5k.pickle","wb")
+pickle.dump(LinearSVC_classifier, save_classifier)
+save_classifier.close()
+
+
+##NuSVC_classifier = SklearnClassifier(NuSVC())
+##NuSVC_classifier.train(training_set)
+##print("NuSVC_classifier accuracy percent:", (nltk.classify.accuracy(NuSVC_classifier, testing_set))*100)
+
+print('running SGDC clf')
+SGDC_classifier = SklearnClassifier(SGDClassifier())
+SGDC_classifier.train(training_set)
+print("SGDClassifier accuracy percent:",nltk.classify.accuracy(SGDC_classifier, testing_set)*100)
+
+save_classifier = open("pickled_algos/SGDC_classifier5k.pickle","wb")
+pickle.dump(SGDC_classifier, save_classifier)
+save_classifier.close()
