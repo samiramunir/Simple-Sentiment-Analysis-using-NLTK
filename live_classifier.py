5601847456ef6303e21eff20ee585aed42117517
LeninGF <enteatenea@gmail.com>
2019-05-17 10:24:23 -0500

    Corrections in live classifier and Addons to SentimentAnalysis  On branch live_classifier  Changes to be committed: 	modified:   Sentiment_Analysis.ipynb I have added a code to download the reported dataset in github I have added a code to extract the .tar.gz file downloaded I have modifed train/pos and train/neg paths since now any user can download the dataset and extract it to local repo I have included nltk downloads commands to download necessary files for processing.
    	modified:   live_classifier.py
    	new file:   pickled_algos/documents.pickle
    	new file:   pickled_algos/word_features5k.pickle
    
    Live_classifier has an error. it calls for an EnsembleClassifier function
    but a VoteClassifier function is actually declared. I renamed the vote
    function to ensemble to correct this.
    
    Also this script asks for documents.pickle which is not in original repo
    I have processed the jupyter notebook to get this file and
    word_features5k
    
    Also I added the function print_feat so that I can print the features
    words. If I miss understood and those are not the features words which
    I interpret as the most relevant words found so that algorithm can tell
    if it is a positive or negative review, please remove that function

--- live_classifier.py
+++ live_classifier.py
@@ -11,7 +11,7 @@ from nltk.tokenize import word_tokenize
 
 
 
-class VoteClassifier(ClassifierI):
+class EnsembleClassifier(ClassifierI):
     def __init__(self, *classifiers):
         self._classifiers = classifiers
 
@@ -37,9 +37,6 @@ documents_f = open("pickled_algos/documents.pickle", "rb")
 documents = pickle.load(documents_f)
 documents_f.close()
 
-
-
-
 word_features5k_f = open("pickled_algos/word_features5k.pickle", "rb")
 word_features = pickle.load(word_features5k_f)
 word_features5k_f.close()
@@ -50,7 +47,6 @@ def parse_text(document):
     features = {}
     for w in word_features:
         features[w] = (w in words)
-
     return features
 
 
@@ -81,13 +77,42 @@ LogReg_Clf = load_model('pickled_algos/LogReg_clf.pickle')
 SGD_Clf = load_model('pickled_algos/SGD_clf.pickle')
 
 
-
 ensemble_clf = EnsembleClassifier(ONB_Clf, MNB_Clf, BNB_Clf, LogReg_Clf, SGD_Clf)
 
 
-
+def print_feats(d):
+    z = []
+    for key in d:
+        if d[key]:
+            z.append(key)
+    return z
 
 
 def sentiment(text):
     feats = parse_text(text)
-    return voted_classifier.classify(feats),voted_classifier.confidence(feats)
+    print(print_feats(feats))
+    return ensemble_clf.classify(feats), ensemble_clf.confidence(feats)
+
+
+text_a = 'This movie was so bad'
+
+# print(sentiment(text_a))
+
+text_a = '''The problem is with the corporate anticulture that controls these productions-and 
+            the fandom-targeted demagogy that they're made to fulfill-which responsible casting 
+                can't overcome alone.'''
+text_b = '''Does it work? The short answer is: yes. There's enough to keep both diehard 
+                Marvel fans and newcomers engaged.'''
+text_c = '''It was lacking, a bit flat, and I'm honestly concerned about how she will enter
+            the Marvel Cinematic Universe...it's so concerned with being a feminist film that 
+            it forgets how to be a superhero movie.'''
+text_d = '''The film may be about women breaking their shackles, but the lead actress feels kept 
+            in check for much of the picture. Humor winds up being provided by Samuel Jackson's Nick 
+            Fury, heart by Lashana Lynch's Maria Rambeau, and pathos by...well, it ain't Larson'''
+text_e = '''"Everything was beautiful and nothing hurt"'''
+
+print('a. ',sentiment(text_a), '\nb. ',
+      sentiment(text_b), '\nc. ',
+      sentiment(text_c), '\nd. ',
+      sentiment(text_d), '\ne. ',
+      sentiment(text_e))
